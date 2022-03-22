eaaa69d0cc2f5f1097b333b7b0998908553a5e60
safoinetl <safoinetouil3@gmail.com>
2022-03-22 15:03:03 +0100

    edit jwt

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -25,9 +25,9 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ required: true })
+  @Prop()
   name: string;
-  @Prop({ required: true })
+  @Prop()
   surname: string;
   @Prop({ required: true, unique: true })
   phone: number;
