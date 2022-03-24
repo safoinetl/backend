b859e6d0ebdecbe1d42c570bc44b591a372c099b
safoinetl <safoinetouil3@gmail.com>
2022-03-24 11:00:42 +0100

    update profile and create deal and stuff like this

--- src/coupon/dto/create-coupon.dto.ts
+++ src/coupon/dto/create-coupon.dto.ts
@@ -1,1 +1,26 @@
-export class CreateCouponDto {}
+import { IsString } from 'class-validator';
+import { category } from 'src/enum/category-enum';
+import { dealType } from 'src/enum/deals-type-enum';
+
+export class CreateCouponDto {
+  @IsString()
+  coupon_name: string;
+  @IsString()
+  old_price: string;
+  @IsString()
+  new_price: string;
+  @IsString()
+  description: string;
+  @IsString()
+  deal_picture: string;
+  @IsString()
+  category: [category];
+  @IsString()
+  coupon_type: dealType;
+  @IsString()
+  date_validation: Date;
+  @IsString()
+  created_date: Date;
+  @IsString()
+  reduc_esti: string;
+}
